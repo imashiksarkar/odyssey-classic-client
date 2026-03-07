a1850f5a8b9d25284ae9a2eec5a4bcaa391b2e3e
Ashik Sarkar <ashiksarkar.s18@gmail.com>
2026-03-07 14:19:18 +0600

    ODY2-110 -- chore: pull origin main

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import sso from "@/config/sso";
 import { CheckCircle2, LogOut } from "lucide-react";
 import { use, useEffect, useState } from "react";
 import { AuthContext } from "./auth-wrapper";
+import Link from "next/link";
 
 const Home = () => {
   const [focus, setFocus] = useState(0);
