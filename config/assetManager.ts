bd901ff23e1d176d500c6af31f208d65bf325c26
Abidstic <abidstic@Abidstics-MacBook-Air.local>
2026-03-15 03:11:00 +0600

    ODY2-109 -- Refactor: update package version

--- config/assetManager.ts
+++ config/assetManager.ts
@@ -3,6 +3,7 @@ import { AssetManagerSDK } from "@newgameplusinc/odyssey-asset-manager-sdk";
 const assetManager = new AssetManagerSDK({
   baseUrl: process.env.NEXT_PUBLIC_API_BASE!,
   timeout: 30000,
+  sdkKey: process.env.NEXT_PUBLIC_ASSET_SDK_KEY,
 });
 
 export default assetManager;
